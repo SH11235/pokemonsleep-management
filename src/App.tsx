9cc904ef508063bfa6241ff71c1049d6122bac97
SH11235 <aabceh1a1a2i3u5o8@gmail.com>
2024-12-22 17:20:30 +0900

    リダイレクト設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { type ReactNode, useEffect } from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    useNavigate,
+    useLocation,
+} from "react-router-dom";
 
 import Layout from "./components/Layout";
 import CalculationList from "./pages/CalculationList";
@@ -7,13 +14,31 @@ import Calculator from "./pages/Calculator";
 const App = () => (
     // vite.config.ts の base に指定したパスを basename に指定する
     <Router basename="/pokemonsleep-management/">
-        <Layout>
-            <Routes>
-                <Route path="/" element={<Calculator />} />
-                <Route path="/list" element={<CalculationList />} />
-            </Routes>
-        </Layout>
+        <RedirectHandler>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Calculator />} />
+                    <Route path="/list" element={<CalculationList />} />
+                </Routes>
+            </Layout>
+        </RedirectHandler>
     </Router>
 );
+
+const RedirectHandler = ({ children }: { children: ReactNode }) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        const redirectPath = params.get("redirect");
+
+        if (redirectPath) {
+            navigate(redirectPath, { replace: true });
+        }
+    }, [location, navigate]);
+
+    return <>{children}</>;
+};
 
 export default App;
